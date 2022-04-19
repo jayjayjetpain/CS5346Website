5c1f0fa1d214eec1826348b5c3661b90c917e350
Jayson Werth <46936772+jayjayjetpain@users.noreply.github.com>
2022-04-18 22:47:53 -0500

    backend server update

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,74 @@
 const express = require('express');
 const path = require('path');
+// const mysql = require('mysql');
+const {MongoClient} = require('mongodb');
 const app = express(),
       bodyParser = require("body-parser");
       port = 3080;
 
+async function main(){
+  app.use(bodyParser.json());
+
+  const uri = "mongodb+srv://student:smucs5346@cs5346-apartmentwebsite.htmdn.mongodb.net/cs5346-apartmentwebsite?retryWrites=true&w=majority";
+  const client = new MongoClient(uri);
+  
+  try {
+      // Connect to the MongoDB cluster
+      await client.connect();
+
+      // Make the appropriate DB calls
+      await  listDatabases(client);
+
+  } catch (e) {
+      console.error(e);
+  } finally {
+      await client.close();
+  }
+
+
+  app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+  });
+  
+  app.listen(port, () => {
+      console.log(`Server listening on the port::${port}`);
+  });
+
+}
+
+async function listDatabases(client){
+  databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+main().catch(console.error);
 // place holder for the data
-const users = [];
+// const logger = log({ console: true, file: false, label: config.name });
 
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+// var connection = mysql.createConnection({
 
+//   user: 'root',
+//   password: 'smucs5346',
+//   database: 'apartments',
+//   socketPath: '/cloudsql/cs-5346-apartmentwebsite:us-central1:cs-5346-apartmentwebsite'
+// });
+// console.log(connection)
+// const users = [];
+
+// app.use(bodyParser.json());
+// // app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+
+// connection.connect(function (err) {
+//   if (err) {
+//     logger.error("Cannot connect to DB!");
+//     logger.error(err.stack);
+//   }
+//   else
+//     logger.info("Connected to the DB!");
+// });
 // app.get('/api/users', (req, res) => {
 //   console.log('api/users called!')
 //   res.json(users);
@@ -22,10 +81,10 @@ app.use(express.static(path.join(__dirname, '../frontend/build')));
 //   res.json("user addedd");
 // });
 
-app.get('/', (req,res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
-});
+// app.get('/', (req,res) => {
+//   // res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+// });
 
-app.listen(port, () => {
-    console.log(`Server listening on the port::${port}`);
-});
+// app.listen(port, () => {
+//     console.log(`Server listening on the port::${port}`);
+// });
